48e016e15006c793e3592df47aeaedd6eaa6e08b
“the574th” <sufyazman@gmail.com>
2019-02-18 13:50:25 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
+function drawRectangle(x,y) {
+    for (var u = 0; u < y; u++) {
+        let rowList = [];
+        for (var i = 0; i < x; i++ ) {
+            if ((u === 0) || (u === (y-1))) {
+                if ((i === 0) || (i === (x-1))) {
+                    rowList.push('o')
+                } else {
+                    rowList.push('-')
+                }
+            } else {
+                if ((i === 0) || (i === (x-1))) {
+                    rowList.push('|')
+                } else {
+                    rowList.push(' ')
+                }
+            }
+        }
+        console.log(rowList.join(''))
+    }
+}
+
+drawRectangle(2,2)
+
+
 // function drawRectangle(x) {
 //     let list = [];
-//     for (i = 0; i < x; i++ ) {
-//         list.push('-')
+//     for (var i = 0; i < x; i++ ) {
+//         if ((i === 0) || (i === (x-1))) {
+//             list.push('o')
+//         } else {
+//             list.push('-')
+//         }
 //     }
 //     console.log(list.join(''))
 // }
 
-// drawRectangle(3)
-
-
-function drawRectangle(x) {
-    let list = [];
-    for (var i = 0; i < x; i++ ) {
-        if ((i === 0) || (i === (x-1))) {
-            list.push('o')
-        } else {
-            list.push('-')
-        }
-    }
-    console.log(list.join(''))
-}
-
-drawRectangle(5)
+// drawRectangle(3)x
